f46ed17aa85cad8c5615b0707c5bc3dd124223b1
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2022-08-13 13:05:26 -0600

    styling

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,14 @@ export default class About extends Component {
       <div className="text">
         <h1> ABOUT </h1>
         <p>
-          Nataliya Nechyporenko is a Ph.D. student at the University of Colorado Boulder (CU Boulder) at the <a href="https://hiro-group.ronc.one/"> Human-Interaction and Robotics</a> (HIRO) lab. She received her Bachelor's Degree in Mechanical Engineering from the University of Connecticut (UConn) and her Master's Degree in Robotics from the University of Nantes (ECN) and Jaume I University (UJI). Her industrial experience spans established industries and small-scale startups. She is motivated to understand how robotic technology can be used as an elegant solution to human and environmental problems. She explores the concepts of active robot capabilities and human-robot interaction. At CU Boulder, her research on tactile perception and social intelligence in robotics is funded by the NSF GRFP.
+          Nataliya Nechyporenko is a Ph.D. student at the University of Colorado Boulder (CU Boulder) at the <a href="https://hiro-group.ronc.one/"> Human-Interaction and Robotics</a> (HIRO) lab. She received her Bachelor's Degree in Mechanical Engineering from the University of Connecticut (UConn) and her Master's Degree in Robotics from the University of Nantes (ECN) and Jaume I University (UJI). Her industrial experience spans established industries and small-scale startups. She is motivated to understand how robotic technology can be used as an elegant solution to life's simple problems. She explores the concepts of active robot capabilities and human-robot interaction. At CU Boulder, her research on tactile perception and social intelligence in robotics is funded by the NSF GRFP.
+        </p>
+        <blockquote>
+          “Human knowledge is never contained in one person. It grows from the relationships we create between each other and the world, and still it is never complete.”
+          ― Paul Kalanithi, When Breath Becomes Air
+        </blockquote>
+        <p>
+          She's a collaborator and a creator. If you have an idea for a project or a prospect, then please write her a few words over e-mail.
         </p>
       </div>
     )
